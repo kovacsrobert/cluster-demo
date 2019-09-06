af33154c7393cef23ca5fa8da23d1b4f3cb22c93
Robert Kovacs <robert.kovacs.centacc@gmail.com>
2019-09-06 21:41:21 +0200

    extracted cluster demo into separate repo

---
+++ cluster-web/src/main/java/hello/web/config/EnvironmentVariables.java
@@ -0,0 +1,10 @@
+package hello.web.config;
+
+public interface EnvironmentVariables {
+
+	String INSTANCE_NAME = "INSTANCE_NAME";
+
+	String DAO_URL = "DAO_URL";
+
+	String TERRACOTTA_HOST = "TERRACOTTA_HOST";
+}
